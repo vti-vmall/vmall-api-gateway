e2a3965c75d739ea481d6bbcf5a0b8d4209dc5ad
tunk <nguyenkhactu1996@gmail.com>
2024-06-05 13:55:09 +0700

    Initial commit

---
+++ src/main/java/vn/edu/vti/vmall/vmall_api_gateway/VmallApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.vti.vmall.vmall_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmallApiGatewayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(VmallApiGatewayApplication.class, args);
+  }
+
+}
---
+++ src/test/java/vn/edu/vti/vmall/vmall_api_gateway/VmallApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.vti.vmall.vmall_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VmallApiGatewayApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
