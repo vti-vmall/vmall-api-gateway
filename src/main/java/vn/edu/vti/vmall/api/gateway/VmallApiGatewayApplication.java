b113e2a4c9d3fcd62783b7add3b8b5c645db12b3
tunk <nguyenkhactu1996@gmail.com>
2024-06-14 09:18:36 +0700

    Change package and dir name

--- src/main/java/vn/edu/vti/vmall/api/gateway/VmallApiGatewayApplication.java
+++ src/main/java/vn/edu/vti/vmall/api/gateway/VmallApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package vn.edu.vti.vmall.vmall_api_gateway;
+package vn.edu.vti.vmall.api.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/vn/edu/vti/vmall/api/gateway/VmallApiGatewayApplicationTests.java
+++ src/test/java/vn/edu/vti/vmall/api/gateway/VmallApiGatewayApplicationTests.java
@@ -1,4 +1,4 @@
-package vn.edu.vti.vmall.vmall_api_gateway;
+package vn.edu.vti.vmall.api.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
